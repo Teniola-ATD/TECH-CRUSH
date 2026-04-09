b5c590e32935a16831c424c1aa4588a2ea5fd77e
unknown <ateniolataye@gmail.com>
2026-04-09 17:10:29 +0100

    This is the entire file of Cohort 6 Tech crush

--- class.js
+++ class.js
@@ -2,16 +2,16 @@ const numbers = [1, 2, 3, 4, 5];
 const doubled = numbers.map(num => num * 2);
 console.log(doubled);
 
-const scores = [250, 300, 200, 400, 180,160];
+const scores = [250, 300, 200, 400, 180, 160];
 const filteredScores = scores.filter(score => score >= 300);
 console.log(filteredScores);
 
-const sum =[2000, 3000, 400, 100, 600];
-const total = sum.reduce(ActiveXObject, curr) => acc + curr, 0);
+const sum = [2000, 3000, 400, 100, 600];
+const total = sum.reduce((acc, curr) => acc + curr, 0);
 console.log(total);
 
 const colors = ["blue", "yellow", "red"];
-//Remove "red"
+// Remove "red"
 const updateColors = colors.filter(color => color !== "red");
 console.log(updateColors);
 
